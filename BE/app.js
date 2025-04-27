4a1234efe0a323f82ae487f9b42e8c24d18520e4
arafatmansuri <mansuriarafat302@gmail.com>
2025-04-27 22:41:41 +0530

    Added connection string before the server starts

--- BE/app.js
+++ BE/app.js
@@ -1,14 +1,9 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const userRouter = require("./src/routes/user.router.js");
 const adminRouter = require("./src/routes/admin.router.js");
 const courseRouter = require("./src/routes/course.router.js");
 require("dotenv").config();
 const app = express();
-mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => console.log("DB connected!"))
-  .catch((err) => console.error("Error connecting to Mongo", err));
 app.get("/", (req, res) => {
   return res.json({ message: "Backend running perfectly" });
 });
--- BE/server.js
+++ BE/server.js
@@ -1,6 +1,13 @@
 const app = require("./app");
+const mongoose = require("mongoose");
 require("dotenv").config();
 const port = process.env.PORT || 3001;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("DB connected!");
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch((err) => console.error("Error connecting to Mongo", err));
