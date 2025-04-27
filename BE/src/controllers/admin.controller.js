dc4fa891ceabb44e5c74f6027a5cbc4e55a3cab2
arafatmansuri <mansuriarafat302@gmail.com>
2025-04-27 23:02:20 +0530

    Add function to generate access and refresh token

--- BE/src/controllers/admin.controller.js
+++ BE/src/controllers/admin.controller.js
@@ -1,4 +1,22 @@
-async function generateAccessAndRefreshToken(adminId) {}
+const Admin = require("../models/admin.model");
+
+async function generateAccessAndRefreshToken(adminId) {
+  try {
+    const user = await Admin.findById(adminId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    const accessToken = await user.generateAccessToken();
+    const refreshToken = await user.generateRefreshToken();
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+    return { accessToken, refreshToken };
+  } catch (err) {
+    return res
+      .status(500)
+      .json({ message: err.message || "Something went wrong from our side" });
+  }
+}
 async function signup(req, res) {}
 async function signin(req, res) {}
 async function refreshAccessAndRefreshToken(req, res) {}
--- BE/src/controllers/user.controller.js
+++ BE/src/controllers/user.controller.js
@@ -1,4 +1,22 @@
-async function generateAccessAndRefreshToken(userId) {}
+const User = require("../models/user.model");
+
+async function generateAccessAndRefreshToken(userId) {
+  try {
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    const accessToken = await user.generateAccessToken();
+    const refreshToken = await user.generateRefreshToken();
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+    return { accessToken, refreshToken };
+  } catch (err) {
+    return res
+      .status(500)
+      .json({ message: err.message || "Something went wrong from our side" });
+  }
+}
 async function signup(req, res) {}
 async function signin(req, res) {}
 async function refreshAccessAndRefreshToken(req, res) {}
