f51dfad10770947f277c158b8a8b9d3ee4962897
arafatmansuri <mansuriarafat302@gmail.com>
2025-04-27 18:36:24 +0530

    Add purchase model and courses model

--- BE/src/models/course.model.js
+++ BE/src/models/course.model.js
@@ -2,14 +2,26 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const CourseSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
-    trim: true,
+const CourseSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    imageUrl: {
+      type: String,
+    },
   },
-  description: {
-    type: String,
-  },
-},
-{timestamps:true});
+  { timestamps: true }
+);
+
+const Course = mongoose.model("Course", CourseSchema);
+module.exports = Course;
---
+++ BE/src/models/purchase.model.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const PurchaseSchema = new Schema({
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  courseId: {
+    type: Schema.Types.ObjectId,
+    ref: "Course",
+  },
+});
