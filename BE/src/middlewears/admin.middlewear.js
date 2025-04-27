4b07029f655b4ef4bb1169b61aa0f874fac24f72
arafatmansuri <mansuriarafat302@gmail.com>
2025-04-27 22:50:53 +0530

    Add user and admin  middlewear

--- BE/src/middlewears/admin.middlewear.js
+++ BE/src/middlewears/admin.middlewear.js
@@ -1,4 +1,24 @@
+const jwt = require("jsonwebtoken");
+const Admin = require("../models/admin.model.js");
+
 async function adminAuth(req, res, next) {
-  next();
+  try {
+    const accessToken = req.cookies.accessToken || req.headers("accessToken");
+    if (!accessToken) {
+      return res.status(401).json({ message: "Unathorized" });
+    }
+    const decodedToken = jwt.verify(accessToken, process.env.accessToken);
+    if (!decodedToken) {
+      return res.status(401).json({ message: "Unathorized" });
+    }
+    const user = await Admin.findById(decodedToken._id);
+    if (!user) {
+      return res.status(401).json({ message: "Unathorized" });
+    }
+    req.user = user;
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: err.message || "Unathorized" });
+  }
 }
 module.exports = { adminAuth };
--- BE/src/middlewears/user.middlewear.js
+++ BE/src/middlewears/user.middlewear.js
@@ -1,4 +1,23 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/user.model.js");
 async function userAuth(req, res, next) {
-  next();
+  try {
+    const accessToken = req.cookies.accessToken;
+    if (!accessToken) {
+      return res.status(401).json({ message: "Unathorized" });
+    }
+    const decodedToken = jwt.verify(accessToken, process.env.accessToken);
+    if (!decodedToken) {
+      return res.status(401).json({ message: "Unathorized" });
+    }
+    const user = await User.findById(decodedToken._id);
+    if (!user) {
+      return res.status(401).json({ message: "Unathorized" });
+    }
+    req.user = user;
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: err.message || "Unathorized" });
+  }
 }
 module.exports = { userAuth };
