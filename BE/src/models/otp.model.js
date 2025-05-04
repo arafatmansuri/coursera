abcf8651cd461f4ef1993e6526375ec4c61f43ec
arafatmansuri <mansuriarafat302@gmail.com>
2025-05-04 10:41:47 +0530

    Add OTP schema and nodemailer setup to send emails

---
+++ BE/src/models/otp.model.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const sendMail = require("../utils/mailer");
+
+const OtpSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "BaseUser",
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  otp: {
+    type: Number,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    expires: 60 * 10,
+  },
+  subject: {
+    type: String,
+    required: true,
+  },
+});
+
+OtpSchema.pre("save", async function (next) {
+  if (this.isNew) {
+    await sendMail(this.email, this.subject, this.username, this.otp);
+  }
+  next();
+});
+const OTP = new mongoose.model("OTP", OtpSchema);
+
+module.exports = OTP;
